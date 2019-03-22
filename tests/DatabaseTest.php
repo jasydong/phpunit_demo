280041e569ae6d944e0c8b4386a344e7078d3b01
Jasy Dong <jasydong@qq.com>
2019-03-22 14:51:34 +0800

    添加获取数据记录测试用例

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -13,7 +13,11 @@ class DatabaseTest extends TestCase
     {
         $this->_pdo = new PDO($GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD']);
         $this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->_pdo->query("CREATE TABLE hello_world (key) VARCHAR(50) NOT NULL)");
+        //创建表结构
+        $query = "CREATE TABLE hello_world (`id` int(10) NOT NULL AUTO_INCREMENT, `key` VARCHAR(64) NOT NULL, PRIMARY KEY (`id`))";
+        $this->_pdo->query($query);
+        //插入记录
+        $this->insertRecord();
     }
 
     /**
@@ -22,5 +26,27 @@ class DatabaseTest extends TestCase
     public function tearDown()
     {
         $this->_pdo->query("DROP TABLE hello_world");
+    }
+
+    /**
+     * 插入记录
+     */
+    public function insertRecord()
+    {
+        $v = md5('hello');
+        $this->_pdo->query("insert into hello_world (`key`) values ('{$v}')");
+    }
+
+    /**
+     * 获取记录测试
+     */
+    public function testFetchRecord()
+    {
+        $sql = "select * from hello_world;";
+        $stmt = $this->_pdo->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
+
+        $this->assertEquals(1, count($rows));
     }
 }
